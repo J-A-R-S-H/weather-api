65e5b8f7b6e24d267d139ec91fa6fb1d278c2dc4
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2024-10-26 19:32:22 -0700

    returned proper response for api key

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/vancouver?unitGroup=metric&key=NVTJGNSGWVFDM7AKGC93LFYCH&contentType=json
-`)
-  .then(function (response) {
-    console.log(response, "response");
-  })
-  .catch(function (error) {
-    console.log(error, "error");
-  });
+async function getApi() {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/vancouver?unitGroup=metric&key=NVTJGNSGWVFDM7AKGC93LFYCH&contentType=json`,
+    {
+      mode: "cors",
+    }
+  );
+  return response
+    .json()
+    .then(function (response) {
+      console.log(response, "response");
+    })
+    .catch(function (error) {
+      console.log(error, "error");
+    });
+}
+
+getApi();
