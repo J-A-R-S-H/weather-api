ff4ce8be028fc150e20b9e581caa4e4eb3c23c1b
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2024-10-26 20:09:27 -0700

    stored all required data in variables

--- script.js
+++ script.js
@@ -9,6 +9,24 @@ async function getApi() {
     .json()
     .then(function (response) {
       console.log(response, "response");
+      resolvedAddress = response.resolvedAddress;
+      tempature = response.days[0].temp;
+      maxTempature = response.days[0].tempmax;
+      minTempature = response.days[0].tempmin;
+      rainProbality = response.days[0].precipprob;
+      humidity = response.days[0].humidity;
+      windSpeed = response.days[0].windspeed;
+
+      console.log(
+        "results",
+        resolvedAddress,
+        tempature,
+        maxTempature,
+        minTempature,
+        rainProbality,
+        humidity,
+        windSpeed
+      );
     })
     .catch(function (error) {
       console.log(error, "error");
